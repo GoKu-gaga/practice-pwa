f953406de184a545dd69f87f0738c81ed89de132
GoKu-gaga <max.yuan.ivan@gmail.com>
2020-03-28 11:38:29 +0800

    4

---
+++ 4.BackgroundSync/src/ui.js
@@ -0,0 +1,20 @@
+const ui = {
+  render(todos) {
+    if (todos.length === 0) {
+      document.getElementById('list').innerHTML = '暂无代办事项'
+    } else {
+      document.getElementById('list').innerHTML = todos.map(function (todo) {
+        return `<div class="item">${todo.name} (#${todo.id})</div>`
+      }).join('')
+    }
+  },
+
+  submit(callback) {
+    const name = document.getElementById('name').value.trim()
+    if (name.length === 0) {
+      alert('代办事项不能为空')
+    } else {
+      callback(name)
+    }
+  }
+}
